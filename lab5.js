9393edc81de34c0c5ca730928367c04e2f919f8f
Puneet Bhardwaj <66798951+Puneet489@users.noreply.github.com>
2023-02-08 16:15:31 +0530

    Create lab5.js

---
+++ lab5.js
@@ -0,0 +1,31 @@
+var a =10;
+var b = 5;
+console.log(a)
+console.log("Welcome to program")
+document.write("Hello world")
+console.log(a+b)
+var x=9
+var y=9
+if(x==y){
+    document.write("true");
+}
+else{
+    document.write("False");
+}
+
+let name = ["name","akjd","adfg"];
+for(i=0; i<name.length; i++){
+    document.write(name[i]);
+}
+
+name.push("a","b");
+name.unshift("z");
+for(i=0; i<name.length; i++){
+    document.write(name[i]);
+}
+console.log(name);
+function abc(fname, lname){
+    var fname;
+    var lname;
+    
+}
